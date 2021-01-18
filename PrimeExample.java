d80fe7179770728cd303376334b336a7f99d64ab
Prashanth Saidugari <37907276+8500205266@users.noreply.github.com>
2021-01-18 11:58:09 +0530

    Update PrimeExample.java

--- PrimeExample.java
+++ PrimeExample.java
@@ -1,6 +1,7 @@
 public class PrimeExample{    
  public static void main(String args[]){    
   int i,m=0,flag=0;      
+  //cjvvcjkvcjkvc
   int n=3;//it is the number to be checked    
   m=n/2;      
   if(n==0||n==1){  
